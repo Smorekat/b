ab8772511cae5aa12e18a9fc91962648fd640f05
Lotus <noneofyour@business.gotit>
2022-02-17 18:19:03 -0800

    fix bullet collisions again | fix wall collision bounce

--- collisions.py
+++ collisions.py
@@ -3,6 +3,9 @@ import maps as m
 import player as p
 import random as rng
 
+collision_alg = lambda subjectx, subjecty, subjectw, subjecth, objectx, objecty, objectw, objecth: True if (subjectx+subjectw > objectx and subjecty+subjecth > objecty) and (subjectx < objectx+objectw and subjecty < objecty + objecth) else False
+
+
 def walls():
     # collide = lambda subjectx, subjecty, subjectw, subjecth, objectx, objecty, objectw, objecth: True if (subjectx > objectx and subjecty > objecty) and (subjectx+subjectw < objectx+objectw and subjecty+subjecth < objecty + objecth) else False
     level = []
@@ -12,11 +15,14 @@ def walls():
         # print(sector)
         # (subjectx > objectx and subjecty > objecty) and (subjectx+subjectw < objectx+objectw and subjecty+subjecth < objecty + objecth)
         # [[block, column], [blockx, blocky, blockw, blockh], [size, color]]
+        # TODO: add collision_alg to this instead
         if (p.user[0][0]+p.user[2][0] > sector[1][0] and p.user[0][1]+p.user[2][1] > sector[1][1]) and (p.user[0][0] < sector[1][0]+sector[1][2] and p.user[0][1] < sector[1][1] + sector[1][3]):
             # p.user[0][0] -= p.velocity[0] + p.user_speed
             # p.user[0][1] -= p.velocity[1] + p.user_speed
-            p.user[0][0] += int(-p.velocity[0] * 10) + p.user_speed #int(p.velocity[0])
-            p.user[0][1] += int(p.velocity[1] * 10) + p.user_speed #int(p.velocity[0])
+
+            p.user[0][0] += int(-p.velocity[0] * 2) #+ p.user_speed    #int(p.velocity[0])
+            p.user[0][1] += int(p.velocity[1] * 2) #+ p.user_speed      #int(p.velocity[0])
+            # BUG: Fix slippery walls
             # BUG: Fix collisions for bullets and player w walls
             # TODO: Add enemy pathfinding
             # TODO: document and format code (comment)
--- enemy.py
+++ enemy.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import bullets as b
 import random as rng
+import collisions as c
 
  
 #mainClock = pg.time.Clock()
@@ -17,7 +18,9 @@ def enemy_health_logic(screen):
 
         for bullet in b.bullets:
             #((mx() > primaryActions.attack[0][0] and my() > primaryActions.attack[0][1]) and (mx() < primaryActions.attack[1][0] and my() < primaryActions.attack[1][1])):
-            if (bullet[0][0] > enemy[0][0] and bullet[0][1] > enemy[0][1]) and (bullet[0][0]+b.bullet_size < enemy[0][0]+enemy_size[0] and bullet[0][1]+b.bullet_size < enemy[0][1]+enemy_size[1]):
+            #[p.user[0][0], p.user[0][1]], [int(p.velocity[0]), int(p.velocity[1])], 0])#p.user[0]
+            if (bullet[0][0]+b.bullet_size > enemy[0][0] and bullet[0][1]+b.bullet_size > enemy[0][1]) and (bullet[0][0] < enemy[0][0]+enemy_size[0] and bullet[0][1] < enemy[0][1]+enemy_size[1]):
+# c.collision_alg(bullet[0][0], bullet[1][1], b.bullet_size, b.bullet_size, enemy[0][0], enemy[0][1], enemy_size[0], enemy_size[1])): #bullet[0][0] > enemy[0][0] and bullet[0][1] > enemy[0][1]) and (bullet[0][0]+b.bullet_size < enemy[0][0]+enemy_size[0] and bullet[0][1]+b.bullet_size < enemy[0][1]+enemy_size[1]):
                 enemy[1] -= b.bullet_damage
                 b.bullets.remove(bullet)
                 if enemy[1] <= 0:
@@ -26,5 +29,5 @@ def enemy_health_logic(screen):
         red = 255 - enemy[1] if red >= 0 and red <= 255 else 0
         green = 0 + enemy[1] if green <= 255 and green >= 0 else 0
         # print(red, green)
-        # pg.draw.rect(screen, (red, green, 20), [enemy[0][0], enemy[0][1], enemy_size[0], enemy_size[1]], 0)
+        pg.draw.rect(screen, (red, green, 20), [enemy[0][0], enemy[0][1], enemy_size[0], enemy_size[1]], 0)
         
